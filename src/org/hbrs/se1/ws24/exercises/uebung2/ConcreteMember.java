396d549ea377ea9d47665a587cdc150e887234fd
laura <laura.benzler@gmai.com>
2024-10-14 15:49:54 +0200

    ConcreteMember klasse erstellt und gefüllt

---
+++ src/org/hbrs/se1/ws24/exercises/uebung2/ConcreteMember.java
@@ -0,0 +1,13 @@
+package org.hbrs.se1.ws24.exercises.uebung2;
+
+public class ConcreteMember implements Member{
+    static int id=0;
+
+    ConcreteMember() {
+        id+=1;
+    }
+
+    public Integer getID(){
+        return id;
+    }
+}
