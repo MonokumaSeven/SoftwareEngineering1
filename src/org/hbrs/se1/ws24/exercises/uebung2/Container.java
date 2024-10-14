420e945f09d2b71ed9c0caf778b14eeb4edd84c3
laura <laura.benzler@gmai.com>
2024-10-14 15:55:52 +0200

    Leere Methoden Container

--- src/org/hbrs/se1/ws24/exercises/uebung2/Container.java
+++ src/org/hbrs/se1/ws24/exercises/uebung2/Container.java
@@ -1,4 +1,22 @@
 package org.hbrs.se1.ws24.exercises.uebung2;
 
 public class Container {
+
+    public void addMember(Member member) {
+
+    }
+
+    public String deleteMember(Member member) {
+        String s="";
+        return s;
+    }
+
+    public void dump(){
+
+    }
+
+    public int size(){
+        int size = 0;
+        return size;
+    }
 }
