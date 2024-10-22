7b72397ebad72d6562a194d3cdd1d48edd4eaf4c
laura <laura.benzler@gmai.com>
2024-10-22 12:08:23 +0200

    uebung3 v2

--- test/org/hbrs/se1/ws24/tests/uebung3/ContainerTest.java
+++ test/org/hbrs/se1/ws24/tests/uebung3/ContainerTest.java
@@ -13,72 +13,61 @@ import java.io.PrintStream;
 
 public class ContainerTest {
 
-    private Container c = null;
+    private Container c;
 
     @BeforeEach
     void setUp(){
-        c = new Container();
+        c = Container.getInstance();
     }
 
 
     @Test
-    public void addTest() {
-        ConcreteMember m1 = new ConcreteMember(0);
-        ConcreteMember m2 = new ConcreteMember(0);
-        ConcreteMember m3 = new ConcreteMember(1);
-
-        assertDoesNotThrow(() -> container.addMember(m1));
-        assertThrows(ContainerException.class, () -> container.addMember(m2));
-        assertDoesNotThrow(() -> container.addMember(m3));
+    public void storeLoadTest() {
+        ConcreteMember m1 = new ConcreteMember(1);
+        ConcreteMember m2 = new ConcreteMember(2);
+        assertEquals(0, c.size());
+        try {
+            c.addMember(m1);
+        } catch (ContainerException e) {
+            e.printStackTrace();
+        }
+        asserEquals(1, c.size());
+        try {
+            c.addMember(m2);
+        } catch (ContainerException e) {
+            e.printStackTrace();
+        }
+        asserEquals(2, c.size());
+        c.store();
+        c.deleteMember(1);
+        assertEquals(1, c.size());
+        c.deleteMember(2);
+        assertEquals(0, c.size());
+        c.load();
+        assertEquals(2, c.size());
     }
+
 
     @Test
-    public void deleteTest() throws ContainerException {
-        ConcreteMember m1 = new ConcreteMember(0);
-
-        assertEquals("Das Member-Objekt mit der ID 0 ist nicht vorhanden!", container.deleteMember(0));
-        container.addMember(m1);
-        assertEquals("Das Member-Objekt mit der ID 0 ist nicht vorhanden!", container.deleteMember(1));
-        assertEquals("Das Member-Objekt mit der ID 0 wurde gelöscht!", container.deleteMember(0));
+    public void notImplementedMongoDBTest() {
+        try {
+            c.setStrategy(new PersistenceStrategyMongoDB<Member>());
+            c.store();
+        } catch (PersistenceException e) {
+            assertEquals(PersistenceException.ExceptionType.ImplementationNotAvailable, e.getExceptionTypeType());
+        }
     }
+
 
     @Test
-    public void dumpTest() throws ContainerException {
-        ConcreteMember m1 = new ConcreteMember(0);
-        ConcreteMember m3 = new ConcreteMember(1);
-
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-        System.setOut(new PrintStream(outContent));
-        container.dump();
-        String expectedOutput1 = "";
-        assertEquals(expectedOutput1, outContent.toString());
-
-        System.setOut(new PrintStream(outContent));
-        container.addMember(m1);
-        container.dump();
-        String expectedOutput2 = "Member (ID = 0)";
-        assertEquals(expectedOutput2, outContent.toString());
-
-        System.setOut(new PrintStream(outContent));
-        container.addMember(m2);
-        container.dump();
-        String expectedOutput3 = "Member (ID = 0)\nMember (ID = 1)";
-        assertEquals(expectedOutput3, outContent.toString());
+    public void noStrategyTest() {
+        try {
+            c.setStrategy(null);
+            c.store();
+        } catch (PersistenceException e) {
+            assertEquals(PersistenceException.ExceptionType.NoStrategyIsSet, e.getExceptionTypeType());
+        }
     }
 
-    @Test
-    public void sizeTest() throws ContainerException {
-        ConcreteMember m1 = new ConcreteMember(0);
-        ConcreteMember m3 = new ConcreteMember(1);
-
-        assertEquals(0, container.size());
-
-        container.addMember(m1);
-        asserEquals(1, container.size());
-
-        container.addMember(m2);
-        asserEquals(2, container.size());
-    }
 
 }
